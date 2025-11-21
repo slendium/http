8af18ed6a39ef2fbdd588294f81e031737890c74
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2025-11-21 19:43:19 +0100

    Updates HttpChar, fixes isTChar() not including digits

--- src/Base/HttpChar.php
+++ src/Base/HttpChar.php
@@ -21,10 +21,12 @@ final class HttpChar {
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 	];
 
-	/** @phpstan-assert-if-true '!'|'#'|'$'|'%'|'&'|'\''|'*'|'+'|'-'|'.'|'^'|'_'|'`'|'|'|'~'|value-of<self::LCALPHA>|value-of<self::UCALPHA> $char */
+	/** @phpstan-assert-if-true '!'|'#'|'$'|'%'|'&'|'\''|'*'|'+'|'-'|'.'|'^'|'_'|'`'|'|'|'~'|value-of<self::LCALPHA>|value-of<self::UCALPHA>|value-of<self::DIGIT> $char */
 	public static function isTChar(string $char): bool {
 		return match($char) {
-			'!', '#', '$', '%', '&', "'", '*', '+', '-', '.', '^', '_', '`', '|', '~' => true,
+			'!', '#', '$', '%', '&', "'", '*', '+', '-', '.', '^', '_', '`', '|', '~',
+			'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+				=> true,
 			default => self::isAlpha($char)
 		};
 	}
