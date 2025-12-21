cfc6c200d1be702efbf26521a556054f0cb17b3b
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2025-12-21 18:32:03 +0100

    Update Uri to better reflect the native Rfc3986 Uri

--- src/Uri.php
+++ src/Uri.php
@@ -3,59 +3,158 @@
 namespace Slendium\Http;
 
 use ArrayAccess;
+use Closure;
 use Countable;
 use Stringable;
 use Traversable;
 
+use Uri\Rfc3986\Uri as Rfc3986Uri;
+
+use Slendium\Http\Base\ParseError;
+use Slendium\Http\Base\Query;
+use Slendium\Http\Base\Result;
+
 /**
- * A URI pointing to an online resource.
+ * An RFC 3986 URI.
  *
- * The `__toString()` implementation MUST return the canonical RFC 3986 representation.
+ * Since the native RFC 3986 Uri object does not support accessing individual query parameters, this
+ * library uses its own abstraction. The methods on this object are very similar to the native Uri
+ * object to facilitate upgrading to it later if features are added that make it a better fit. A follow-up
+ * RFC is [under discussion](https://wiki.php.net/rfc/uri_followup) which does include support for query
+ * parameters. However, at the time of writing this implementation seems to be mutable. This would
+ * EITHER mean that every part of the program that uses them can modify the original object OR that
+ * a new object is created at every point of use. Neither of these cases are ideal.
  *
- * Since sending passwords in URL's is deprecated it was not included as its own property. You can
- * append the `$userInfo` property with a `:` to indicate an empty password (this is still allowed).
+ * However, if a native RFC-compliant query parameter parser becomes available it should be used in
+ * favor of the `$_QUERY` superglobal. For now, query parameters are implemented using the native
+ * functions `parse_str` and `http_build_query`.
  *
- * The PHP 8.5+ native `Uri` type did not fit all use cases of this library, so this separate
- * interface was kept with the release of PHP 8.5. Most notably it does not have a way to access or
- * edit individual query parameters (since these are not well defined by standards). Additionally
- * it keeps the deprecated "password" option around (for valid reasons).
+ * Since the "password" option is deprecated by RFC 3986, it was not included in this object. An empty
+ * password is still allowed and can be indicated by suffixing the "userInfo" property with ':'.
  *
  * @see https://www.rfc-editor.org/rfc/rfc3986.html
  *
  * @since 1.0
+ * @phpstan-type QueryParams ArrayAccess<non-empty-string,array<mixed>|string|null>&Countable&Stringable&Traversable<non-empty-string,array<mixed>|string>
  * @author C. Fahner
  * @copyright Slendium 2025
  */
-interface Uri extends Stringable {
-
-	/**
-	 * @since 1.0
-	 * @var ?non-empty-string
-	 */
-	public ?string $scheme { get; }
+final class Uri {
 
 	/** @since 1.0 */
-	public ?string $userInfo { get; }
+	public static function fromString(string $input): Error|self {
+		$result = Result::try(static fn() => new Rfc3986Uri($input));
+		if ($result->hasFailed) {
+			return new ParseError('Could not parse URI: '.$result->getThrowable()->getMessage());
+		}
+		$uri = $result->getResult();
+		return new self($uri, $uri->getQuery() !== null
+			? Query::fromString($uri->getQuery())
+			: null
+		);
+	}
 
 	/**
 	 * @since 1.0
-	 * @var ?non-empty-string
+	 * @param ?int<0,max> $port
+	 * @param ?QueryParams $query
 	 */
-	public ?string $host { get; }
+	public static function create(
+		?string $scheme = null,
+		?string $userInfo = null,
+		?string $host = null,
+		?int $port = null,
+		string $path = '',
+		(ArrayAccess&Countable&Stringable&Traversable)|null $query = null,
+		?string $fragment = null,
+	): Error|self {
+		$result = Result::try(static function() use ($scheme, $userInfo, $host, $port, $path, $query, $fragment) {
+			return new Rfc3986Uri('')
+				->withScheme($scheme)
+				->withUserInfo($userInfo)
+				->withHost($host)
+				->withPort($port)
+				->withPath($path)
+				->withQuery($query)
+				->withFragment($fragment);
+		});
+		return $result->hasSucceeded
+			? new self($result->getResult(), $query)
+			: new ParseError('Could not create URI: '.$result->getThrowable()->getMessage());
+	}
+
+	private function __construct(
+
+		private Rfc3986Uri $uri,
+
+		/** @var ?QueryParams */
+		private (ArrayAccess&Countable&Stringable&Traversable)|null $query,
+
+	) { }
+
+	/** @since 1.0 */
+	public function getScheme(): ?string {
+		return $this->uri->getScheme();
+	}
+
+	/** @since 1.0 */
+	public function withScheme(?string $scheme): ?self {
+		return $this->withUriOrNull(fn() => $this->uri->withScheme($scheme));
+	}
+
+	/** @since 1.0 */
+	public function getUserInfo(): ?string {
+		return $this->uri->getUserInfo();
+	}
+
+	/** @since 1.0 */
+	public function getUsername(): ?string {
+		return $this->uri->getUsername();
+	}
+
+	/** @since 1.0 */
+	public function withUserInfo(?string $userInfo): ?self {
+		return $this->withUriOrNull(fn() => $this->uri->withUserInfo($userInfo));
+	}
+
+	/** @since 1.0 */
+	public function getHost(): ?string {
+		return $this->uri->getHost();
+	}
+
+	/** @since 1.0 */
+	public function withHost(?string $host): ?self {
+		return $this->withUriOrNull(fn() => $this->uri->withHost($host));
+	}
 
 	/**
 	 * @since 1.0
-	 * @var ?int<0,65535>
+	 * @return ?int<0,65535>
 	 */
-	public ?int $port { get; }
+	public function getPort(): ?int {
+		$port = $this->uri->getPort();
+		return $port === null || ($port >= 0 && $port <= 65535)
+			? $port
+			: null;
+	}
 
 	/**
-	 * The path, starting with `/`.
-	 *
 	 * @since 1.0
-	 * @var non-empty-string
+	 * @param ?int<0,max> $port
 	 */
-	public ?string $path { get; }
+	public function withPort(?int $port): ?self {
+		return $this->withUriOrNull(fn() => $this->uri->withPort($port));
+	}
+
+	/** @since 1.0 */
+	public function getPath(): string {
+		return $this->uri->getPath();
+	}
+
+	/** @since 1.0 */
+	public function withPath(string $path): ?self {
+		return $this->withUriOrNull(fn() => $this->uri->withPath($path));
+	}
 
 	/**
 	 * The query data encoded in the URL.
@@ -70,9 +169,23 @@ interface Uri extends Stringable {
 	 * Query arguments are case-sensitive according to [RFC 3986](https://www.rfc-editor.org/rfc/rfc3986#section-6.2.2.1).
 	 *
 	 * @since 1.0
-	 * @var (ArrayAccess<non-empty-string,array<mixed>|string|null>&Countable&Stringable&Traversable<non-empty-string,array<mixed>|string>)|null
+	 * @return ?QueryParams
 	 */
-	public (ArrayAccess&Countable&Stringable&Traversable)|null $query { get; }
+	public function getQuery(): (ArrayAccess&Countable&Stringable&Traversable)|null {
+		return $this->query;
+	}
+
+	/**
+	 * @since 1.0
+	 * @param QueryParams|string|null $query
+	 */
+	public function withQuery((ArrayAccess&Countable&Stringable&Traversable)|string|null $query): self {
+		return \clone($this, [
+			'query' => \is_string($query)
+				? Query::fromString($query)
+				: $query
+		]);
+	}
 
 	/**
 	 * The hash/fragment part of the URL, without the `#`.
@@ -81,6 +194,21 @@ interface Uri extends Stringable {
 	 *
 	 * @since 1.0
 	 */
-	public ?string $fragment { get; }
+	public function getFragment(): ?string {
+		return $this->uri->getFragment();
+	}
+
+	/** @since 1.0 */
+	public function withFragment(?string $fragment): ?self {
+		return $this->withUriOrNull(fn() => $this->uri->withFragment($fragment));
+	}
+
+	/** @param Closure():Rfc3986Uri $witherCall */
+	private function withUriOrNull(Closure $witherCall): ?self {
+		$newUri = Result::tryOrNull($witherCall);
+		return $newUri !== null
+			? \clone($this, [ 'uri' => $newUri ])
+			: null;
+	}
 
 }
