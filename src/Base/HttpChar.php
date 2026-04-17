fbe9d4295c45033ac46da3246f0393a29215465c
C. Fahner <cfahner.git.ixuk9phi@slendium.org>
2026-04-17 20:10:11 +0200

    Refactor HttpChar to ensure non-empty-string arguments

--- src/Base/HttpChar.php
+++ src/Base/HttpChar.php
@@ -5,7 +5,7 @@ namespace Slendium\Http\Base;
 /**
  * @internal
  * @author C. Fahner
- * @copyright Slendium 2025
+ * @copyright Slendium 2025-2026
  */
 final class HttpChar {
 
@@ -21,7 +21,10 @@ final class HttpChar {
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
 	];
 
-	/** @phpstan-assert-if-true '!'|'#'|'$'|'%'|'&'|'\''|'*'|'+'|'-'|'.'|'^'|'_'|'`'|'|'|'~'|value-of<self::LCALPHA>|value-of<self::UCALPHA>|value-of<self::DIGIT> $char */
+	/**
+	 * @param non-empty-string $char
+	 * @phpstan-assert-if-true '!'|'#'|'$'|'%'|'&'|'\''|'*'|'+'|'-'|'.'|'^'|'_'|'`'|'|'|'~'|value-of<self::LCALPHA>|value-of<self::UCALPHA>|value-of<self::DIGIT> $char
+	 */
 	public static function isTChar(string $char): bool {
 		return match($char) {
 			'!', '#', '$', '%', '&', "'", '*', '+', '-', '.', '^', '_', '`', '|', '~',
@@ -31,29 +34,42 @@ final class HttpChar {
 		};
 	}
 
+	/** @param non-empty-string $char */
 	public static function isControl(string $char): bool {
 		return \ord($char) <= 31;
 	}
 
-	/** @phpstan-assert-if-true value-of<self::LCALPHA>|value-of<self::UCALPHA> $char */
+	/**
+	 * @param non-empty-string $char
+	 * @phpstan-assert-if-true value-of<self::LCALPHA>|value-of<self::UCALPHA> $char
+	 */
 	public static function isAlpha(string $char): bool {
 		$char = \ord($char);
 		return ($char >= 65 && $char <= 90) || ($char >= 97 && $char <= 122);
 	}
 
-	/** @phpstan-assert-if-true value-of<self::LCALPHA> $char */
+	/**
+	 * @param non-empty-string $char
+	 * @phpstan-assert-if-true value-of<self::LCALPHA> $char
+	 */
 	public static function isLowercaseAlpha(string $char): bool {
 		$char = \ord($char);
 		return $char >= 97 && $char <= 122;
 	}
 
-	/** @phpstan-assert-if-true value-of<self::DIGIT> $char */
+	/**
+	 * @param non-empty-string $char
+	 * @phpstan-assert-if-true value-of<self::DIGIT> $char
+	 */
 	public static function isDigit(string $char): bool {
 		$char = \ord($char);
 		return $char >= 48 && $char <= 57;
 	}
 
-	/** @phpstan-assert-if-true '0'|'1' $char */
+	/**
+	 * @param non-empty-string $char
+	 * @phpstan-assert-if-true '0'|'1' $char
+	 */
 	public static function isBit(string $char): bool {
 		$char = \ord($char);
 		return $char === 48 || $char === 49;
